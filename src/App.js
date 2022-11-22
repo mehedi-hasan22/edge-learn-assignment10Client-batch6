9415a523d637fe21dc955aaa54c174b44e9e38e8
mehedi-hasan22 <mehedi30554@gmail.com>
2022-11-23 04:24:22 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './Components/Blog/Blog';
+import Courses from './Components/Courses/Courses';
+import FAQ from './Components/FAQ/FAQ';
+import Home from './Components/Home/Home';
+import Main from './layout/Main';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/courses',
+          element: <Courses></Courses>
+        },
+        {
+          path: '/faq',
+          element: <FAQ></FAQ>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        },
+        {
+          path: '*',
+          element: <div className='text-center'>
+            <h1>Sorry</h1>
+            <p>The route you want to visit doesn't exist. please try again!</p>
+            <p>error code: 404</p>
+          </div>
+        }
+      ]
+    },
+  ])
+
   return (
     <div >
-      <h1>Hello DaisyUI</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
