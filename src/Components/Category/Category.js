b750f1f21a4b751f575846af8bef0053d43846f7
mehedi-hasan22 <mehedi30554@gmail.com>
2022-11-24 00:49:23 +0600

    designed dynamic

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -4,9 +4,23 @@ import { useLoaderData } from 'react-router-dom';
 const Category = () => {
     const category = useLoaderData();
     const data = category[0]
+    console.log(data)
     return (
-        <div>
+        <div className='w-9/12 mx-auto'>
             <h1 className='text-3xl text-center'>dynamic category for: {data.title}</h1>
+            <div className='mx-auto border rounded-md m-5 p-5'>
+                <img className='mx-auto w-full rounded-md mb-4' src={data.image_url} alt="" />
+                <p>{data.details}</p>
+                <h1>Course Outline</h1>
+                <div className='ms-5'>
+                    <li>lifetime excess</li>
+                    <li>Pre recorded class</li>
+                    <li>Quiz after class</li>
+                    <li>24/7 support</li>
+                    <li>Course finishing Certificate</li>
+                </div>
+                <button className='btn btn-primary my-5'>Get Premium</button>
+            </div>
         </div>
     );
 };
